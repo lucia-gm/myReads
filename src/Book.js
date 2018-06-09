187680b6b5a5c03550b4a4a4067992a84aba4256
Lucia Gonzalez Moscoso <luciagonzalezmoscoso@gmail.com>
2018-06-08 19:06:11 -0700

    feat: List of books is dynamically generated

---
+++ src/Book.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import ShelfSelector from './ShelfSelector.js'
+
+class Book extends Component {
+  render() {
+    return (
+      <li>
+        <div className="book">
+          <div className="book-top">
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.img})` }}></div>
+            <ShelfSelector shelf={this.props.shelf} />
+          </div>
+          <div className="book-title">{this.props.title}}</div>
+          <div className="book-authors">{this.props.author}</div>
+        </div>
+      </li>
+    )
+  }
+}
+
+
+export default Book
