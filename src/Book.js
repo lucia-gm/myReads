04788296884d9a5b54200f81f81afb21eeccf87e
Lucia Gonzalez Moscoso <luciagonzalezmoscoso@gmail.com>
2018-06-11 15:11:52 -0700

    style: Removes duplicated curly bracket

--- src/Book.js
+++ src/Book.js
@@ -10,7 +10,7 @@ class Book extends Component {
             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.img})` }}></div>
             <ShelfSelector book={this.props.book} shelf={this.props.shelf} onUpdateShelf={this.props.onUpdateShelf}/>
           </div>
-          <div className="book-title">{this.props.title}}</div>
+          <div className="book-title">{this.props.title}</div>
           <div className="book-authors">{this.props.author}</div>
         </div>
       </li>
