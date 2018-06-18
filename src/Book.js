922983af67b13808119ac96b1ce31abb03202fd0
Lucia Gonzalez Moscoso <luciagonzalezmoscoso@gmail.com>
2018-06-18 10:36:28 -0700

    refactor: Store reusable code in variables

--- src/Book.js
+++ src/Book.js
@@ -3,15 +3,16 @@ import ShelfSelector from './ShelfSelector.js'
 
 class Book extends Component {
   render() {
+    const { book, shelf, onUpdateShelf, title, author } = this.props
     return (
       <li>
         <div className="book">
           <div className="book-top">
             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.img})` }}></div>
-            <ShelfSelector book={this.props.book} shelf={this.props.shelf} onUpdateShelf={this.props.onUpdateShelf}/>
+            <ShelfSelector book={book} shelf={shelf} onUpdateShelf={onUpdateShelf}/>
           </div>
-          <div className="book-title">{this.props.title}</div>
-          <div className="book-authors">{this.props.author}</div>
+          <div className="book-title">{title}</div>
+          <div className="book-authors">{author}</div>
         </div>
       </li>
     )
