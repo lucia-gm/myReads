01872a2d0e52fd553ce4750c48cd107173ec0421
Lucia Gonzalez Moscoso <luciagonzalezmoscoso@gmail.com>
2018-06-18 11:01:31 -0700

    refactor: Book and ShelfSelector to stateless components

--- src/Book.js
+++ src/Book.js
@@ -1,23 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
 import ShelfSelector from './ShelfSelector.js'
 
-class Book extends Component {
-  render() {
-    const { book, shelf, onUpdateShelf, title, author } = this.props
-    return (
-      <li>
-        <div className="book">
-          <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.img})` }}></div>
-            <ShelfSelector book={book} shelf={shelf} onUpdateShelf={onUpdateShelf}/>
-          </div>
-          <div className="book-title">{title}</div>
-          <div className="book-authors">{author}</div>
-        </div>
-      </li>
-    )
-  }
-}
-
+const Book = (props) => ( 
+  <li>
+    <div className="book">
+      <div className="book-top">
+        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${props.img})` }}></div>
+        <ShelfSelector book={props.book} shelf={props.shelf} onUpdateShelf={props.onUpdateShelf}/>
+      </div>
+      <div className="book-title">{props.title}</div>
+      <div className="book-authors">{props.author}</div>
+    </div>
+  </li>
+)
 
 export default Book
--- src/ShelfSelector.js
+++ src/ShelfSelector.js
@@ -1,21 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class ShelfSelector extends Component {
-  render() {
-    return (
-      <div className="book-shelf-changer">
-        <select value={this.props.shelf} onChange={ event => {
-            this.props.onUpdateShelf(this.props.book, event.target.value)
-          }}>
-          <option value="move" disabled>Move to...</option>
-          <option value="currentlyReading">Currently Reading</option>
-          <option value="wantToRead">Want to Read</option>
-          <option value="read">Read</option>
-          <option value="none">None</option>
-        </select>
-      </div>
-    )
-  }
-}
-
+const ShelfSelector = (props) => (
+  <div className="book-shelf-changer">
+    <select value={props.shelf} onChange={ event => {
+        props.onUpdateShelf(props.book, event.target.value)
+      }}>
+      <option value="move" disabled>Move to...</option>
+      <option value="currentlyReading">Currently Reading</option>
+      <option value="wantToRead">Want to Read</option>
+      <option value="read">Read</option>
+      <option value="none">None</option>
+    </select>
+  </div>
+)
 export default ShelfSelector
